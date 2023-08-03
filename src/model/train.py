829fde4e697683c2b9b5a0857f0b7fdd19b4b610
louis@programmer <62738677+louis103@users.noreply.github.com>
2023-08-03 15:19:20 +0300

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -37,8 +37,7 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 
-               'TricepsThickness',
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',
                 'SerumInsulin', 'BMI', 'DiabetesPedigree',
                 'Age']].values, df['Diabetic'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, 
@@ -66,7 +65,7 @@ def parse_args():
 
     # return args
     return args
-    
+
 
 # run script
 if __name__ == "__main__":
@@ -83,4 +82,3 @@ if __name__ == "__main__":
     # add space in logs
     print("*" * 60)
     print("\n\n")
-
